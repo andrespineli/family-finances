ae0be101f1b8f73c0671adf9d489dc03a5c99c41
Andre Spineli <andrespineli@gmail.com>
2019-06-23 20:49:01 -0300

    Validando nome  no cadastro de familias

--- src/Controller/FamilyController.php
+++ src/Controller/FamilyController.php
@@ -6,16 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Family;
 use App\Form\NewFamilyFormType;
+use Symfony\Component\HttpFoundation\Request;
 
 class FamilyController extends AbstractController
 {
     /**
      * @Route("/family/new", name="new_family")
      */
-    public function new()
+    public function new(Request $request)
     {
         $family = new Family;
         $form = $this->createForm(NewFamilyFormType::class, $family);
+        $form->handleRequest($request);       
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
--- src/Entity/Family.php
+++ src/Entity/Family.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity
@@ -18,6 +20,7 @@ class Family
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Family Name is required.")
      */
     private $name;
 
